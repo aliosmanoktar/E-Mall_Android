c0179c78d984cfdf2dd8c06b513c7dba50ef50a0
aliosman <aliosmanoktar@gmail.com>
2019-04-26 02:55:05 +0300

    Urun Listeleme Eklendi

---
+++ app/src/main/java/com/aliosman/emall/Adapter/adapter_urun_list.java
@@ -0,0 +1,69 @@
+package com.aliosman.emall.Adapter;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import com.aliosman.emall.Interface.RecylerItemClick;
+import com.aliosman.emall.Model.Get.Urun;
+import com.aliosman.emall.R;
+import com.squareup.picasso.Picasso;
+import java.util.List;
+import jp.shts.android.library.TriangleLabelView;
+
+public class adapter_urun_list extends RecyclerView.Adapter<adapter_urun_list.ViewHolder> {
+    List<Urun> items;
+    RecylerItemClick<Urun> click;
+
+    public adapter_urun_list(List<Urun> items, RecylerItemClick<Urun> click) {
+        this.items = items;
+        this.click = click;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder( ViewGroup viewGroup, int i) {
+        View view= LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.item_urun_listeleme,viewGroup,false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder( ViewHolder viewHolder, int i) {
+        Urun item = items.get(i);
+        viewHolder.Fiyat.setText(item.getFiyat()+ " ₺");
+        viewHolder.UrunAdi.setText(item.getAdi());
+        if (item.getOran()>0 && item.getEskiFiyat()!=0){
+            viewHolder.IndirimOran.setVisibility(View.VISIBLE);
+            viewHolder.EskiFiyat.setVisibility(View.VISIBLE);
+            viewHolder.EskiFiyat.setText(item.getEskiFiyat()+ " ₺");
+            viewHolder.IndirimOran.setPrimaryText("%"+item.getOran());
+        }
+        Picasso.get()
+                .load(item.getResimler().get(0))
+                .centerCrop().fit()
+                .into(viewHolder.UrunImage);
+    }
+
+    @Override
+    public int getItemCount() {
+        return items.size();
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder{
+        private TriangleLabelView IndirimOran;
+        private TextView UrunAdi,EskiFiyat,Fiyat;
+        private ImageView UrunImage;
+        private LinearLayout layout;
+        public ViewHolder(View itemView) {
+            super(itemView);
+            IndirimOran=itemView.findViewById(R.id.item_urun_list_indirimOran);
+            UrunAdi=itemView.findViewById(R.id.item_urun_list_urunAdi);
+            EskiFiyat=itemView.findViewById(R.id.item_urun_list_urunEskiFiyat);
+            Fiyat=itemView.findViewById(R.id.item_urun_list_urunFiyat);
+            UrunImage=itemView.findViewById(R.id.item_urun_list_urunImage);
+            layout=itemView.findViewById(R.id.item_indirim_urun_layout);
+        }
+    }
+}
